d9c97fa8e8452b573d90d0e330dcbb5fed50002a
DmitryF <solarman.awm@gmail.com>
2023-06-26 17:57:29 +0300

    Add basic header and footer

---
+++ src/widgets/footer/ui/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<script lang="ts">
+export default {
+    name: 'AppFooter',
+};
+</script>
+
+<template>
+    <footer>
+        <p>footer</p>
+    </footer>
+</template>
---
+++ src/widgets/header/ui/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<script lang="ts">
+export default {
+    name: 'AppHeader',
+};
+</script>
+
+<template>
+    <header>
+        <p>header</p>
+    </header>
+</template>
