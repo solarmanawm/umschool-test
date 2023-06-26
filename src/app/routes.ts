fabfade4513b71962b78a1c93b8ab1ec010c9df0
DmitryF <solarman.awm@gmail.com>
2023-06-26 18:23:57 +0300

    Adjust router view components

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -9,25 +9,31 @@ export const routes = {
     films: {
         path: '/films',
         name: 'films',
+        component: () => import('@/pages/films/ui/index.vue'),
     },
     people: {
         path: '/people',
         name: 'people',
+        component: () => import('@/pages/people/ui/index.vue'),
     },
     planets: {
         path: '/planets',
         name: 'planets',
+        component: () => import('@/pages/planets/ui/index.vue'),
     },
     species: {
         path: '/species',
         name: 'species',
+        component: () => import('@/pages/species/ui/index.vue'),
     },
     starships: {
         path: '/starships',
         name: 'starships',
+        component: () => import('@/pages/starships/ui/index.vue'),
     },
     vehicles: {
         path: '/vehicles',
         name: 'vehicles',
+        component: () => import('@/vehicles/ui/index.vue'),
     },
 };
