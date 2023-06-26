69eeb1cc564dadb55b9620749768e72e05942bc0
DmitryF <solarman.awm@gmail.com>
2023-06-26 18:26:55 +0300

    Move main app files

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/app/style.css'
+import App from '@/App.vue'
 import { router } from '@/app/router';
 
 createApp(App)
