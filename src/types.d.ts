cca7ae551e45c8cec9f05dfb696e3797f68a55fc
DmitryF <solarman.awm@gmail.com>
2023-06-27 19:47:14 +0300

    Define a character interface

--- src/types.d.ts
+++ src/types.d.ts
@@ -12,3 +12,25 @@ interface Film {
     species: string[];
     url: string;
 }
+
+interface Character {
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: string[];
+    species: string[];
+    vehicles: string[];
+    starships: string[];
+    url: string;
+}
+
+interface Detail {
+    title: string;
+    link: string;
+}
