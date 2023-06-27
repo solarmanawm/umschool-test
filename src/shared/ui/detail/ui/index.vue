dacb4f8708e31f2a0f775ce51693cbc69236abdf
DmitryF <solarman.awm@gmail.com>
2023-06-27 19:46:27 +0300

    Add a detail component

---
+++ src/shared/ui/detail/ui/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AppAnchor from '@/shared/ui/anchor/ui/index.vue';
+
+interface Props {
+    title: string;
+    link: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<script lang="ts">
+export default {
+    name: 'AppDetail',
+};
+</script>
+
+<template>
+    <div class="flex items-center justify-between border-b border-b-gray-700 p-4 text-gray-400 hover:bg-gray-800">
+        <span>{{ title }}</span>
+        <app-anchor class="text-white" :to="link">Details</app-anchor>
+    </div>
+</template>
