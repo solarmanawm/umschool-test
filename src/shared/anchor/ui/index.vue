6c6b4ccd45fa124b95a66920d7ac34aeeee4b6bc
DmitryF <solarman.awm@gmail.com>
2023-06-27 11:45:03 +0300

    Add anchor component

---
+++ src/shared/anchor/ui/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+    href?: string;
+    to?: string;
+}
+
+const props = defineProps<Props>();
+const component = props.href ? 'a' : 'router-link';
+</script>
+
+<template>
+    <component
+        :is="component"
+        v-bind="props"
+        class="underline hover:no-underline"
+    >
+        <slot></slot>
+    </component>
+</template>
