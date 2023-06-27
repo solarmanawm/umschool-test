f324dd03e0af1366a357ca2dc57ddc0d896e951f
DmitryF <solarman.awm@gmail.com>
2023-06-27 19:55:03 +0300

    Add getCharacterDetails fn

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,3 +1,15 @@
 export const getId = (url: string) => {
     return url.split('/').filter(e => !!e).pop();
 };
+
+export const getCharacterDetails = (characters: Character[]) => {
+    return characters.filter(e => !!e).map((character) => {
+        const { name: title, url } = character;
+        const detail: Detail = {
+            title,
+            link: `/people/${getId(url)}`,
+        };
+
+        return detail;
+    });
+}
