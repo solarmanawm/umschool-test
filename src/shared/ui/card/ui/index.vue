747a436d07c3b3c6a7e010c9bab2ed9753a2c508
DmitryF <solarman.awm@gmail.com>
2023-06-27 16:47:34 +0300

    Add a card component

---
+++ src/shared/ui/card/ui/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+export default {
+    name: 'AppCard',
+};
+</script>
+
+<template>
+    <div class="border border-gray-800 p-6 text-left shadow-xl relative z-10 hover:shadow-2xl bg-gray-800 rounded text-gray-400">
+        <slot></slot>
+    </div>
+</template>
