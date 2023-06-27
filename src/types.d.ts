2cce2a90dd8eb5ff8b80acc51c7274ad19aaca43
DmitryF <solarman.awm@gmail.com>
2023-06-27 17:23:31 +0300

    Extend film interface

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,14 @@
 interface Film {
-    episode_id: string;
+    title: string;
+    episode_id: number;
+    opening_crawl: string;
+    director: string;
+    producer: string;
+    release_date: string;
+    characters: string[];
+    planets: string[];
+    starships: string[];
+    vehicles: string[];
+    species: string[];
+    url: string;
 }
