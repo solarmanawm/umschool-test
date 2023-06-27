1555c520721424f5a41e3d1ea7ffda7d56a93932
DmitryF <solarman.awm@gmail.com>
2023-06-27 16:53:06 +0300

    Build a basic fils markup

--- src/pages/films/ui/index.vue
+++ src/pages/films/ui/index.vue
@@ -2,13 +2,17 @@
 import { onBeforeMount, ref } from 'vue';
 import AppLayout from '@/shared/ui/layout/ui/index.vue';
 import AppPreloader from '@/shared/ui/preloader/ui/index.vue';
+import AppColumn from '@/shared/ui/column/ui/index.vue';
+import AppContainer from '@/shared/ui/container/ui/index.vue';
+import AppRow from '@/shared/ui/row/ui/index.vue';
+import AppCard from '@/shared/ui/card/ui/index.vue';
 import { useRoute } from 'vue-router';
 import { getFilms } from '@/shared/api/films';
 
 const route = useRoute();
 
 const isLoading = ref(true);
-const films = ref([])
+const films = ref<Film[]>([])
 
 onBeforeMount(async () => {
     const { results } = await getFilms();
@@ -22,7 +26,22 @@ onBeforeMount(async () => {
         <template #title v-if="route.meta.title">{{ route.meta.title }}</template>
         <template #content>
             <app-preloader :isLoading="isLoading">
-                <pre>{{ films }}</pre>
+                <app-container>
+                    <app-row class="-mt-4">
+                        <app-column
+                            v-for="film of films"
+                            sm="12"
+                            md="4"
+                            lg="4"
+                            xl="4"
+                            class="mt-4"
+                        >
+                            <app-card>
+                                <p>{{ film.episode_id }}</p>
+                            </app-card>
+                        </app-column>
+                    </app-row>
+                </app-container>
             </app-preloader>
         </template>
     </app-layout>
