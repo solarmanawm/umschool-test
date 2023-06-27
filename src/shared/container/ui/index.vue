bd651202ee5f313e0393fff5a1e9f4c0e7880f65
DmitryF <solarman.awm@gmail.com>
2023-06-27 11:37:06 +0300

    Add container component

---
+++ src/shared/container/ui/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full mx-auto px-2 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
+        <slot></slot>
+    </div>
+</template>
+  
+<script lang="ts">
+export default {
+    name: "AppContainer",
+};
+</script>
+  
