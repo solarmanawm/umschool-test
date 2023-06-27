7ba1b0d33d12383f42b84b4c058c30d6bdb084bb
DmitryF <solarman.awm@gmail.com>
2023-06-27 14:35:25 +0300

    Add preloader component

---
+++ src/shared/ui/preloader/ui/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppSpinner from '@/shared/ui/spinner/ui/index.vue';
+interface Props {
+    isLoading: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<script lang="ts">
+export default {
+    name: 'AppPreloader',
+};
+</script>
+
+<template>
+    <template v-if="isLoading">
+        <div class="w-full h-60 flex items-center justify-center">
+            <app-spinner class="animate-spin" />
+        </div>
+    </template>
+    <template v-else>
+        <slot></slot>
+    </template>
+</template>
