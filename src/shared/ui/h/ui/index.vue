3a04cca443a59729a13cf4689af33aca9d85e2bc
DmitryF <solarman.awm@gmail.com>
2023-06-27 17:23:11 +0300

    Add a html header component

---
+++ src/shared/ui/h/ui/index.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { h, defineComponent } from 'vue';
+
+const hClassList = {
+   1: 'text-4xl',
+   2: 'text-3xl',
+   3: 'text-2xl',
+   4: 'text-xl',
+   5: 'text-lg',
+   6: 'text-base',
+};
+
+export default defineComponent({
+   render() {
+      const classList = [
+         hClassList[this.level as keyof typeof hClassList],
+         'font-bold',
+      ];
+
+      return h(`h${this.level}`, { classList }, this.$slots.default!());
+   },
+
+   props: {
+      level: {
+         type: Number,
+         required: true
+      },
+   },
+});
+</script>
