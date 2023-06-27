0bd8b22451d853cec0fd8f9a3f18ad23dade71b6
DmitryF <solarman.awm@gmail.com>
2023-06-27 18:43:17 +0300

    Add a single film api method

--- src/shared/api/films.ts
+++ src/shared/api/films.ts
@@ -18,3 +18,21 @@ export const getFilms = async (): Promise<FilmsResponse> => {
         };
     }
 };
+
+export const getFilmById = async (id: number) => {
+    try {
+        const response = await axios.get(`/films/${id}`);
+
+        return response.data;
+    } catch (error: any) {
+        const dummyResponse = await import('@/dummy/films');
+        const { results } = dummyResponse.default;
+        const film = results.find((film: Film) => {
+            const currentId = film.url.split('/').filter(e => !!e).pop();
+
+            return currentId === id.toString();
+        });
+
+        return film;
+    }
+};
