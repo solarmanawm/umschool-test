da8fea670f2ee48ba07f21e818748cf0346507f5
DmitryF <solarman.awm@gmail.com>
2023-06-27 14:36:44 +0300

    Extend axios instance

--- src/app/axios.ts
+++ src/app/axios.ts
@@ -4,7 +4,10 @@ const BASE_URL = 'https://swapi.dev/api/';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    timeout: 1000,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default axiosInstance;
