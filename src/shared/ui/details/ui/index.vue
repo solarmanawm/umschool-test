9978cb751dcfee05ff2f8e83029bf1920ce6b203
DmitryF <solarman.awm@gmail.com>
2023-06-27 19:46:40 +0300

    Add a common details component

---
+++ src/shared/ui/details/ui/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import AppDetail from '@/shared/ui/detail/ui/index.vue';
+
+interface Props {
+    details: Detail[],
+}
+
+const props = defineProps<Props>();
+</script>
+
+<script lang="ts">
+export default {
+    name: 'AppDetails',
+};
+</script>
+
+<template>
+    <div class="border-t border-t-gray-700">
+        <app-detail
+            v-for="detail of details"
+            :key="detail.title"
+            :title="detail.title"
+            :link="detail.link"
+        />
+    </div>
+</template>
