dfa4c945a8b0f2d9fe1e76f07c2d99372193c9e9
DmitryF <solarman.awm@gmail.com>
2023-06-27 13:17:50 +0300

    Add a common page layout component

--- src/pages/films/ui/index.vue
+++ src/pages/films/ui/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import AppLayout from '@/shared/layout/ui/index.vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 </script>
 
 <template>
-    <div>{{ route.meta.title }}</div>
+    <app-layout>
+        <template #title v-if="route.meta.title">{{ route.meta.title }}</template>
+        <template #content>
+            <p>content</p>
+        </template>
+    </app-layout>
 </template>
---
+++ src/shared/layout/ui/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useSlots, computed } from 'vue';
+
+const slots = useSlots();
+const hasTitle = computed(() => slots.title && slots.title().length > 0);
+</script>
+
+<script lang="ts">
+export default {
+    name: 'AppLayout',
+};
+</script>
+
+<template>
+    <div class="my-12">
+        <div
+            v-if="hasTitle"
+            class=""
+        >
+            <h1 class="text-white text-4xl">
+                <slot name="title"></slot>
+            </h1>
+        </div>
+        <div class="">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
