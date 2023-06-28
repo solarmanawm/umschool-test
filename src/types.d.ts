5d5732b56723480440d12225b236ef595dd7e392
DmitryF <solarman.awm@gmail.com>
2023-06-28 08:43:48 +0300

    Define additional interfaces

--- src/types.d.ts
+++ src/types.d.ts
@@ -13,6 +13,22 @@ interface Film {
     url: string;
 }
 
+interface Species {
+    average_height: string;
+    average_lifespan: string;
+    classification: string;
+    designation: string;
+    eye_colors: string;
+    hair_colors: string;
+    homeworld: string;
+    language: string;
+    name: string;
+    people: string[];
+    films: string[];
+    skin_colors: string;
+    url: string;
+}
+
 interface Character {
     name: string;
     height: string;
@@ -33,4 +49,55 @@ interface Character {
 interface Detail {
     title: string;
     link: string;
+}
+
+interface Vehicle {
+    cargo_capacity: string;
+    consumables: string;
+    cost_in_credits: string;
+    crew: string;
+    length: string;
+    manufacturer: string;
+    max_atmosphering_speed: string;
+    model: string;
+    name: string;
+    passengers: string;
+    pilots: string[];
+    films: string[];
+    url: string;
+    vehicle_class: string;
+}
+
+interface Starship {
+    MGLT: string;
+    cargo_capacity: string;
+    consumables: string;
+    cost_in_credits: string;
+    crew: string;
+    hyperdrive_rating: string;
+    length: string;
+    manufacturer: string;
+    max_atmosphering_speed: string;
+    model: string;
+    name: string;
+    passengers: string;
+    films: string[];
+    pilots: string[];
+    starship_class: string;
+    url: string;
+}
+
+interface Planet {
+    climate: string;
+    diameter: string;
+    films: string[];
+    gravity: string;
+    name: string;
+    orbital_period: string;
+    population: string;
+    residents: string[];
+    rotation_period: string;
+    surface_water: string;
+    terrain: string;
+    url: string;
 }
