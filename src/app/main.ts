f40f21ba281107a37f62bfc7c96a6b347f304ad3
DmitryF <solarman.awm@gmail.com>
2023-06-30 08:37:59 +0300

    Use pinia

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import '@/app/style.css'
 import App from '@/App.vue'
 import { router } from '@/app/router';
 
+const pinia = createPinia()
+
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app');
